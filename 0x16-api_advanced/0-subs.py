e5ebe207bee04811588bc799238c8dc3f467529e
Amukelani <111043334+Amukelani5587@users.noreply.github.com>
2024-06-06 00:51:20 +0200

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Exporting csv files"""
+import json
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """Read reddit API and return number subscribers """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        return (r.json()["data"]["subscribers"])
+    else:
+        return(0)
